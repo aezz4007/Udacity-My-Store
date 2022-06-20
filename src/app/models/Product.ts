bfa676cc2e679e8a86d7a1c7963f0b21fcbee087
Amira Ezzeldin <aezz4007@gmail.com>
2022-06-20 16:33:42 +0200

    feat: Create product list component interface

---
+++ src/app/models/Product.ts
@@ -0,0 +1,17 @@
+import { SafeUrl } from "@angular/platform-browser";
+
+export class Product {
+    id: number;
+    name: string;
+    price: number;
+    url: SafeUrl;
+    description: string;
+
+    constructor() {
+        this.id = 0;
+        this.name = '';
+        this.price  = 0;
+        this.url = 'https://www.example.com';
+        this.description = '';
+    }
+}
